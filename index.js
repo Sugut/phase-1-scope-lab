398ea0eb93362f10430d30abd50adf8766fa7065
Sugut <sugutvictorine@gmail.com>
2022-08-20 23:22:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer= "sugut"
+ 
+
+var upperCaseCustomerName=()=>{return customerName=customerName.toUpperCase()};
+
+var setBestCustomer=()=>{return bestCustomer= "not bob"};
+
+var overwriteBestCustomer=()=>{return bestCustomer= "maybe bob"};
+
+var changeLeastFavoriteCustomer=()=>{return leastFavoriteCustomer="bob marley"};
